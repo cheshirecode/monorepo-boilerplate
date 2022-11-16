1a2f762849bcc1d52d8236a73f45b931142c57fa
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-16 19:35:26 +0800

    Create helper.tsx

---
+++ packages/app-1/src/test/helper.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/export */
+import { cleanup, render } from '@testing-library/react';
+import { afterEach } from 'vitest';
+import { SWRConfig } from 'swr';
+import ErrorBoundary from '@/components/ErrorBoundary';
+
+afterEach(() => {
+  cleanup();
+});
+
+const customRender = (ui: React.ReactElement, options = {}) =>
+  render(ui, {
+    // wrap provider(s) here if needed
+    wrapper: ({ children }) => children,
+    ...options
+  });
+
+// override render export
+export { customRender as render };
+
+export const NoCacheWrapper: BaseFC = ({ children }) => (
+  <ErrorBoundary>
+    <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>{children}</SWRConfig>
+  </ErrorBoundary>
+);
