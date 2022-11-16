ffd97d2e7925336faebfb2714c59296e0e39c23c
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-16 19:34:08 +0800

    Update vite.config.ts

--- packages/app-1/vite.config.ts
+++ packages/app-1/vite.config.ts
@@ -5,14 +5,12 @@ import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import Unocss from '@unocss/vite';
-import presetWindi from '@unocss/preset-wind';
+import unocssConfig from './unocss.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Unocss({
-      presets: [presetWindi()]
-    }),
+    Unocss({}, unocssConfig ),
     react()
   ],
   resolve: {
@@ -20,10 +18,31 @@ export default defineConfig({
       '@': resolve(__dirname, './src')
     }
   },
+  base: '/',
+  clearScreen: false,
+  build: {
+    // skip minification to make tests faster
+    minify: config.mode !== 'test' ? 'esbuild' : false,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            const vendorModules = ['react', 'lodash', '@emotion', '@gs-ux-uitoolkit'];
+            const m = vendorModules.find((x) => id.includes(x)) ?? '';
+            return `vendor${m ? `-${m}` : ''}`;
+          }
+        }
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.js'],
+    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: [...configDefaults.exclude, 'src/test/**/*'],
     coverage: {
       reporter: ['text', 'lcov']
