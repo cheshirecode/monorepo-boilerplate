2f6906714e201e9fc7bfe29b001f4afb54d2c363
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-22 02:38:26 +0800

    Update Error.tsx

--- packages/app-1/src/components/Error.tsx
+++ packages/app-1/src/components/Error.tsx
@@ -8,15 +8,21 @@ const Error: FC<BaseProps & { text?: string; errors?: unknown[] }> = ({
   errors,
   ...props
 }) => (
-  <div className={cx('flex bg-red-500 p-4 rounded-md flex justify-center', className)} {...props}>
+  <div
+    className={cx('border-0 flex flex-col bg-red-500 p-4 rounded-md justify-center', className)}
+    {...props}
+  >
     {children ? children : null}
-    {text ? <h4 className="text-white text-h4">{text}</h4> : null}
+    {text ? <h4 className="text-white text-h4 text-center">{text}</h4> : null}
     {errors ? (
       <>
-        <h2 className="w-full align-middle text-center self-center text-h1 text-white">Error</h2>
-        {errors.map((x) => (
-          <pre>{JSON.stringify(x, null, 2)}</pre>
-        ))}
+        <ul className="w-full text-h1 text-white list-none flex flex-wrap gap-2">
+          {errors.map((x) => (
+            <li className="" key={x?.toString() ?? x}>
+              <pre>{JSON.stringify(x, null, 2)}</pre>
+            </li>
+          ))}
+        </ul>
       </>
     ) : null}
   </div>
