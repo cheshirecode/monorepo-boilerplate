2495f1072a8f793c668400e443d80182ea34f7af
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-22 02:42:26 +0800

    Create Field.stories.tsx

---
+++ packages/app-1/src/components/Field.stories.tsx
@@ -0,0 +1,53 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Field from './Field';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'components/Field',
+  component: Field
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof Field>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Field> = (args) => (
+  <fieldset className="h-20 w-50">
+    <Field {...args} />
+  </fieldset>
+);
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  name: 'name',
+  value: 'default',
+  // eslint-disable-next-line no-console
+  set: (v) => console.log(v),
+  iconClassName: '',
+  className: 'font-gs-body01 outline outline-solid',
+  readonly: false
+};
+
+export const BigIcon = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+BigIcon.args = {
+  name: 'name',
+  value: 'big icon',
+  // eslint-disable-next-line no-console
+  set: (v) => console.log(v),
+  iconClassName: 'h-10 w-10',
+  className: 'h-10 font-gs-body01 outline outline-solid',
+  readonly: false
+};
+
+export const Readonly = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Readonly.args = {
+  name: 'name',
+  value: 'readonly',
+  // eslint-disable-next-line no-console
+  set: (v) => console.log(v),
+  iconClassName: '',
+  className: 'font-gs-body01 outline outline-solid',
+  readonly: true
+};
