75769d78bba05035024beaac89bd7a3842431176
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-22 02:44:00 +0800

    Create Pagination.stories.tsx

---
+++ packages/app-1/src/components/Pagination.stories.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Pagination from './Pagination';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'components/Pagination',
+  component: Pagination,
+  args: {
+    itemsPerPage: 5,
+    itemClassName: 'cursor-pointer hover:underline',
+    activeItemClassName: 'bg-blue-50 text-white disabled',
+    count: 20
+  }
+} as ComponentMeta<typeof Pagination>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {};
+
+export const ExternalCallback: ComponentStory<typeof Pagination> = (props) => {
+  const [params, setParams] = useState({
+    page: 1,
+    first: 0,
+    last: 0
+  });
+  return (
+    <section>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+      <Pagination
+        onChange={(_e, params) => {
+          setParams(params);
+        }}
+        {...props}
+      />
+    </section>
+  );
+};
+ExternalCallback.args = {};
