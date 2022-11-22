cab5b4e468995d7e07bac3ed6812d60ee2b04d2a
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-22 14:26:05 +0800

    big update

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   env: {
     browser: true,
+    node: true,
     'shared-node-browser': true
   },
   plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'unused-imports'],
@@ -18,7 +19,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:import/recommended',
@@ -56,7 +56,13 @@ module.exports = {
       }
     ],
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off'
+    // // allow jsx syntax in js files (for next.js project)
+    // 'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', 'tsx'] }] //should add ".ts" if typescript project
   },
   settings: {
     react: {
