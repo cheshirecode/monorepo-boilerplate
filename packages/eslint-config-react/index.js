7645ecf87a83b2d691ca00a2a39098cc043d6a01
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-22 10:40:22 +0800

    Update index.js

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -16,10 +16,10 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
     'prettier',
-    'react-app'
+    'react-app',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -30,6 +30,34 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', 
+          'external', 
+          'internal',
+          ['sibling', 'parent'], 
+          'index',
+          'unknown'
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
     ],
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
