93ab8da75844ba8e70f2bd4664b90bd3c3292f35
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-11-30 03:00:07 +0800

    Fix bugs
    improve Pagination
    
    clean up pagination

---
+++ packages/app-1/src/components/Pagination/hooks.ts
@@ -0,0 +1,113 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+import { getIntervals } from '@/utils';
+
+import type { PaginationHookResults, PaginationInputs, PaginationParams } from './';
+
+export const DEFAULT_PAGINATION_THRESHOLD = 10;
+const createNewParams: (
+  p: PaginationInputs
+) => PaginationParams & Required<Pick<PaginationInputs, 'page'>> = ({
+  page,
+  count,
+  pageSize = false
+}) => {
+  if (count < 0) {
+    // eslint-disable-next-line no-console
+    console.error('pageSize', pageSize, 'count', count);
+    throw RangeError('invalid props, both pageSize and count need to be integers');
+  }
+
+  /**
+   * pageSize is +ve integer, use it
+   * pageSize === false - use the full list
+   * pageSize !== false and not +ve integer, take the 1st page size option
+   * */
+  const threshold = Math.max(count, DEFAULT_PAGINATION_THRESHOLD);
+  const intervals = getIntervals([], threshold);
+  const n = (
+    pageSize === false || pageSize === Math.abs(~~pageSize) ? [~~pageSize || threshold] : intervals
+  )[0];
+  // compute page size options for easy references
+  const pageSizes =
+    pageSize === Math.abs(~~pageSize)
+      ? [...new Set(intervals.concat(pageSize).filter((x) => !!x))]
+      : intervals;
+  pageSizes.sort((a, b) => a - b);
+
+  const maxPage = Math.max(Math.ceil(count / n), 1);
+  const newPage = Math.max(Math.min(Number(page), maxPage), 1);
+  const first = Math.min((newPage - 1) * n, count);
+  const last = Math.max(Math.min(count, first + n) - 1, maxPage ? 1 : 0);
+
+  return {
+    page: newPage,
+    count,
+    last,
+    first,
+    maxPage,
+    pageNumbers: Array(maxPage)
+      .fill(0)
+      .map((_v, i) => i + 1),
+    pageSize: n,
+    pageSizes
+  } as const;
+};
+
+export const usePagination: (p: PaginationInputs) => PaginationHookResults = (props) => {
+  const { page: initialPage = 1, pageSize, count, onChange, isRollover = true } = props ?? {};
+  const [params, _setParams] = useState(
+    createNewParams({
+      page: initialPage,
+      count,
+      pageSize
+    })
+  );
+  const setParams = useCallback<PaginationHookResults['setParams']>(
+    (p) => {
+      const newParams = createNewParams({
+        ...params,
+        ...p
+      });
+      _setParams(newParams);
+      if (typeof onChange === 'function') {
+        onChange(newParams);
+      }
+    },
+    [onChange, params]
+  );
+
+  const helpers: PaginationHookResults = useMemo(
+    () => ({
+      ...params,
+      setParams,
+      isNextPossible: isRollover || params.page > 1,
+      isPrevPossible: isRollover || params.page < params.maxPage,
+      isRollover,
+      goTo: (page: number) => {
+        setParams({ page });
+      },
+      onPageNumberClick: (e) => setParams({ page: Number(e.currentTarget.dataset.id) }),
+      goPrevious: () => {
+        setParams({ page: params.page <= 1 ? params.maxPage : params.page - 1 });
+      },
+      goNext: () => {
+        setParams({ page: params.page >= params.maxPage ? 1 : params.page + 1 });
+      },
+      setPageSize: (pageSize: number) => setParams({ pageSize })
+    }),
+    [params, setParams, isRollover]
+  );
+
+  useEffect(() => {
+    helpers.goTo(initialPage);
+    setParams({
+      page: initialPage,
+      count,
+      pageSize
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialPage, count, pageSize]);
+
+  return helpers;
+};
---
+++ packages/app-1/src/components/Pagination/mock.ts
@@ -0,0 +1,34 @@
+import { faker } from '@faker-js/faker';
+
+export type Person = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  createdAt: Date;
+};
+const i = 0;
+const newPerson = (): Person => {
+  return {
+    id: i + 1,
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    age: faker.datatype.number(60),
+    createdAt: faker.datatype.datetime({ max: new Date().getTime() })
+  };
+};
+
+export function makeData(...lens: number[]) {
+  const makeDataLevel = (depth = 0): Person[] => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const len = lens[depth]!;
+    return Array(len)
+      .fill(0)
+      .map(() => ({
+        ...newPerson(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined
+      }));
+  };
+
+  return makeDataLevel();
+}
