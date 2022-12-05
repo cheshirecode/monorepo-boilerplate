f2ee232dd1c4780f6aca4929aa35a507f9dd7007
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-12-05 22:09:31 +0800

    Board

---
+++ packages/app-1/src/components/Board/index.tsx
@@ -0,0 +1,32 @@
+import cx from 'classnames';
+
+export type TagsProps = BaseProps & {
+  items: string[];
+  middleSpan?: number;
+  itemClassName?: string;
+};
+
+const Board = ({ items, middleSpan = 1, className, itemClassName }: TagsProps) => (
+  <p
+    className={cx(
+      'grid',
+      // https://github.com/unocss/unocss/issues/1478#issuecomment-1229132066
+      middleSpan === 2 && 'grid-cols-[auto_1fr_1fr_auto]',
+      middleSpan === 3 && 'grid-cols-[auto_1fr_1fr_1fr_auto]',
+      ![2, 3].includes(middleSpan) && 'grid-cols-[auto_1fr_auto]',
+      'children:(inline-block px-2)',
+      className
+    )}
+  >
+    {items.map((x, i) => (
+      <span
+        key={i}
+        className={cx(!x && 'bg-transparent text-transparent', 'text-center', x && itemClassName)}
+      >
+        {x}
+      </span>
+    ))}
+  </p>
+);
+
+export default Board;
