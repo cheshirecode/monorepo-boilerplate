8cd627baf428f2cf35a3ab45e257a69da88109dc
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-12-31 20:08:36 +0800

    Create useDrilldownTracker.ts

---
+++ packages/app-1/src/services/hooks/useDrilldownTracker.ts
@@ -0,0 +1,21 @@
+import { useCallback, useState } from 'react';
+
+export type DrilldownTrackerHookResults = {
+  readonly opened: Record<string, boolean>;
+  readonly toggleOpened: (e: MouseEvent<HTMLButtonElement>) => void;
+};
+
+const useDrilldownTracker = (): DrilldownTrackerHookResults => {
+  const [opened, setOpened] = useState<DrilldownTrackerHookResults['opened']>({});
+  const toggleOpened = useCallback<DrilldownTrackerHookResults['toggleOpened']>((e) => {
+    const qs = e.currentTarget.dataset.id;
+    setOpened((x) => ({
+      ...x,
+      [qs]: !(x[qs] ?? false)
+    }));
+  }, []);
+
+  return { opened, toggleOpened } as const;
+};
+
+export default useDrilldownTracker;
