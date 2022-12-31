36a0dfa09ff1c4aa53c5b418ad34f718bd5bcd9f
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2022-12-31 20:07:39 +0800

    Update fetch.ts

--- packages/app-1/src/services/api/fetch.ts
+++ packages/app-1/src/services/api/fetch.ts
@@ -1,7 +1,8 @@
+import stringify from 'fast-json-stable-stringify';
+import fetch from 'isomorphic-unfetch';
 import { merge } from 'lodash-es';
-import useSWR from 'swr';
 import type { Key, SWRConfiguration, SWRHook, SWRResponse } from 'swr';
-import fetch from 'unfetch';
+import useSWR, { useSWRImmutable } from 'swr';
 
 export type Fetcher = <T>(url: string, options?: RequestInit) => Promise<T>;
 export type SWRMiddleware<T> = (
@@ -10,6 +11,14 @@ export type SWRMiddleware<T> = (
 
 // no typing for error limitation https://github.com/microsoft/TypeScript/issues/6283#issuecomment-240804072
 export const getRequest: Fetcher = async (url, options = {}) => {
+  // flag specific for testing with happy-dom
+  // NOTE - in browser, process.env is undefined so only check in vitest/jest env (for headless browser, rethink)
+  const isTestingInHappyDom = import.meta.env.MODE === 'test' && !!process?.env?.NODE_HAPPY_DOM;
+
+  const isImage = options?.headers ? options?.headers['Content-Type']?.startsWith('image/') : false;
+  if (isImage) {
+    return getBlob(url, options);
+  }
   // @ts-expect-error
   const res = await fetch(url, {
     ...options,
@@ -18,11 +27,30 @@ export const getRequest: Fetcher = async (url, options = {}) => {
   // 200 response
   /* c8 ignore next 8 */
   if (res.ok) {
-    return res.json();
+    return !isTestingInHappyDom ? await res?.json() : res;
   }
   // very hard to test error handling
   const error: ErrorHttp = new Error(res.statusText); // non-2xx HTTP responses into errors
-  error.info = await res.json();
+  // flag specific for testing with happy-dom
+  error.info = !isTestingInHappyDom ? await res?.json() : stringify(res);
+  error.status = res.status;
+  return error;
+};
+
+export const getBlob: Fetcher = async (url, options = {}) => {
+  // @ts-expect-error
+  const res = await fetch(url, {
+    ...options,
+    credentials: 'include'
+  });
+  // 200 response
+  /* c8 ignore next 8 */
+  if (res.ok) {
+    return res.blob();
+  }
+  // very hard to test error handling
+  const error: ErrorHttp = new Error(res.statusText); // non-2xx HTTP responses into errors
+  error.info = await res.blob();
   error.status = res.status;
   return error;
 };
@@ -43,13 +71,11 @@ export const useGet = <T>(key: Key, config?: SWRConfiguration) =>
   useSWR<T, ErrorHttp>(key, getRequest<T>, config);
 
 export const usePost = <T>(key: Key, options: RequestInit, config?: SWRConfiguration) =>
-  useSWR<T, ErrorHttp>(
+  useSWRImmutable<T, ErrorHttp>(
     'POST ' + key,
     (u: Key, o: RequestInit) => postRequest<T>(String(key), merge(options, o)),
     {
-      ...config,
-      refreshInterval: 0,
-      revalidateIfStale: false
+      ...config
     }
   );
 
@@ -67,10 +93,8 @@ export const logger =
     );
 logger.stdoutMessagePrefix = 'SWR Request:';
 
-export const createHeaderMiddleware: <T>(headers: RequestInit['headers']) => SWRMiddleware<T> =
-  <T>(headers: RequestInit['headers']) =>
-  (useSWRNext) =>
-  (key, fetcher, config) =>
+export const createHeaderMiddleware = <T>(headers: RequestInit['headers']) =>
+  ((useSWRNext) => (key, fetcher, config) =>
     useSWRNext<T, ErrorHttp>(
       key,
       (...args: Parameters<Fetcher>) =>
@@ -84,4 +108,74 @@ export const createHeaderMiddleware: <T>(headers: RequestInit['headers']) => SWR
           )
         ),
       config
+    )) as SWRMiddleware<T>;
+
+const _defaultIsValid = <T>(_d: T) => true;
+
+export const validateResponse = <T>(
+  data: T | ErrorHttp,
+  error: ErrorHttp | undefined,
+  isValid: (d: T) => boolean = _defaultIsValid<T>,
+  sampleData: T
+) => {
+  const errorMessages = [];
+
+  if (!isValid(data)) {
+    if (data?.info?.message) {
+      errorMessages.push({
+        title: 'Unexpected error',
+        status: data?.status,
+        message: data.info.message,
+        ...(!import.meta.env.PROD ? { objects: [data] } : {})
+      });
+    } else {
+      errorMessages.push({
+        title: 'Invalid data',
+        objects: [
+          {
+            // more accurate than typeof
+            expected: Object.prototype.toString.call(sampleData),
+            actual: Object.prototype.toString.call(data)
+          }
+        ]
+      });
+    }
+  }
+
+  if (error?.message) {
+    const { message, status, ...rest } = error;
+    errorMessages.push({
+      title: message,
+      status,
+      ...(!import.meta.env.PROD ? { errors: [rest] } : {})
+    });
+  }
+
+  return errorMessages;
+};
+/**
+ * validate API responses and return well-formed error messages
+ *
+ * @param isValid
+ * @param defaultValue
+ * @returns
+ */
+export const createValidationMiddleware = <T>(
+  isValid: (d: T) => boolean = _defaultIsValid<T>,
+  defaultValue: T
+) =>
+  ((useSWRNext) => (key, fetcher, config) => {
+    const { data, error, isLoading, ...rest } = useSWRNext<T, ErrorHttp>(
+      key,
+      (...args: Parameters<Fetcher>) => fetcher<T>(...args),
+      config
     );
+    const errorMessages = validateResponse<T>(data, error, isValid, defaultValue);
+    return {
+      data: errorMessages.length ? defaultValue : data,
+      error: errorMessages.length ? data : error,
+      isLoading,
+      errorMessages,
+      ...rest
+    } as const;
+  }) as SWRMiddleware<T>;
