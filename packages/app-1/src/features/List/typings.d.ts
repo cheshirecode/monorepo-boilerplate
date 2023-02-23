bfe8790eeda60a753ac02e498127d6d7b0b7f00b
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-02-23 22:40:53 +0800

    Update typings.d.ts

--- packages/app-1/src/features/List/typings.d.ts
+++ packages/app-1/src/features/List/typings.d.ts
@@ -1,5 +1,4 @@
-import type { PaginationInputs, PaginationStyleProps } from '@/components/Pagination/typings';
-import type { TableProps } from '@/components/Table/typings';
+import type { PaginationHookParams } from '@/components/Pagination/typings';
 
 export type ListParams<T> = {
   filter: {
@@ -7,12 +6,6 @@ export type ListParams<T> = {
     onChange?: (str: string) => void;
     fn?: (arr: T[], str: string) => T[];
   };
-  pagination: Partial<PaginationInputs>;
-};
-
-export type ListProps<T> = BaseProps & {
-  pagination: PaginationStyleProps & ListParams['pagination'];
-  data: T[];
-} & {
-  table: Omit<TableProps<T>, 'data'>;
+  pagination: Partial<PaginationHookParams>;
+  postProcess?: (arr: T[]) => T[];
 };
