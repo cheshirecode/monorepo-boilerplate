e17ccff334e231e8fb25acd47fbcdee5c60cde23
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-02-23 22:38:58 +0800

    Update index.tsx

--- packages/app-1/src/components/Tags/index.tsx
+++ packages/app-1/src/components/Tags/index.tsx
@@ -1,15 +1,20 @@
 import cx from 'classnames';
+import { isFunction } from 'lodash-es';
+import type { ReactNode } from 'react';
 
 export type TagsProps = BaseProps & {
-  items: string[];
+  items: (ReactNode | ((p: TagsProps) => ReactNode))[];
   itemClassName?: string;
 };
 
-const Tags = ({ items, className, itemClassName }: TagsProps) => (
-  <ul className={cx('w-full', 'flex flex-wrap gap-2', className)}>
-    {items?.map((p) => (
-      <li key={p} className={cx('inline-block', itemClassName)}>
-        {p}
+const Tags = ({ items, className, itemClassName, ...rest }: TagsProps) => (
+  <ul className={cx('flex flex-wrap gap-2', 'w-full', 'p-0 m-0', className)} {...rest}>
+    {items?.map((c) => (
+      <li
+        key={c}
+        className={cx('w-fit inline-block', 'py-1 px-2', 'border rounded-full', itemClassName)}
+      >
+        {isFunction(c) ? c() : c}
       </li>
     ))}
   </ul>
