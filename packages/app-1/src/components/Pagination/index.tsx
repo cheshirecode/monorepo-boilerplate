0d7a5e8ff532ba20327dccdeaa109e3e68e8b4e0
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-02-23 22:32:44 +0800

    Update index.tsx

--- packages/app-1/src/components/Pagination/index.tsx
+++ packages/app-1/src/components/Pagination/index.tsx
@@ -1,5 +1,4 @@
 import cx from 'classnames';
-import { Fragment, useMemo } from 'react';
 
 import Field from '@/components/Field';
 
@@ -9,14 +8,14 @@ import usePagination from './usePagination';
 export const PlainPagination = ({
   // skip those from internal hook
   children: _children,
-  pageSize: _pageSize,
-  count: _count,
   onChange: _onChange,
   setParams: _setParams,
-  setPageSize: _setPageSize,
-  pageSizes: _pageSizes,
-  first: _first,
-  last: _last,
+  setPageSize,
+  pageSize,
+  pageSizes,
+  first: _f,
+  last: _l,
+  count: _c,
   // necessary props
   className,
   itemClassName,
@@ -30,126 +29,131 @@ export const PlainPagination = ({
   isPrevPossible,
   goPrevious,
   isNextPossible,
+  goTo: _g,
   goNext,
-  goTo,
+  goFirst,
+  goLast,
+  goToAttempt,
   showAllPages = false,
+  isTotalLessThanMinPageSize,
   // DOM-only props
   ...props
 }: PaginationProps) => {
-  const callbacks = useMemo(
-    () => ({
-      goFirst: () => goTo(1),
-      goLast: () => goTo(maxPage),
-      set: (v: unknown) => {
-        const newV = pageNumbers.find((x) => x === Number(v)) ?? pageNumbers[0];
-        goTo(newV);
-        return newV;
-      }
-    }),
-    [goTo, maxPage, pageNumbers]
-  );
-
   return (
-    <Fragment>
-      {maxPage <= 1 && null}
-      {maxPage > 1 && (
-        <div
-          className={cx(
-            'my-4',
-            'flex flex-gap-2',
-            'w-fit mx-auto',
-            'children:(p-2 cursor-pointer transition-all-200)',
-            className
-          )}
-          {...props}
-        >
-          <button
-            key="first"
-            tabIndex={0}
-            onClick={callbacks.goFirst}
-            disabled={!isPrevPossible && !isRollover}
+    !isTotalLessThanMinPageSize && (
+      <section className="flex my-4 min-h-10">
+        {maxPage > 1 && (
+          <div
             className={cx(
-              'btn btn-secondary',
-              !isPrevPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
-              itemClassName
+              'flex-1 flex flex-gap-2 justify-center',
+              'w-fit mx-auto',
+              'children:(p-2 cursor-pointer transition-all-200)',
+              className
             )}
+            {...props}
           >
-            {'<<'}
-          </button>
-          <button
-            key="prev"
-            tabIndex={0}
-            onClick={goPrevious}
-            disabled={!isPrevPossible && !isRollover}
-            className={cx(
-              'btn btn-secondary',
-              !isPrevPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
-              itemClassName
-            )}
-          >
-            {'<'}
-          </button>
-          {!showAllPages && (
-            <Field
-              name="--pagination-page"
-              value={String(page)}
-              title={`Page 1..${maxPage}`}
-              displayValue={(v) => `${v} / ${maxPage}`}
-              set={callbacks.set}
+            <button
+              key="first"
+              tabIndex={0}
+              onClick={goFirst}
+              disabled={!isPrevPossible && !isRollover}
               className={cx(
-                'uno-layer-o:(py-0 hover-none inline-block)',
-                'uno-layer-o:(my-auto w-fit)'
+                'btn btn-secondary',
+                !isPrevPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
+                itemClassName
               )}
-              inputClassName="uno-layer-o:w-15ch"
-              readOnlyClassName=""
-            />
-          )}
-          {showAllPages &&
-            pageNumbers.map((v) => (
-              <button
-                key={v}
-                tabIndex={0}
-                data-id={v}
-                onClick={onPageNumberClick}
-                disabled={v === page}
+            >
+              {'<<'}
+            </button>
+            <button
+              key="prev"
+              tabIndex={0}
+              onClick={goPrevious}
+              disabled={!isPrevPossible && !isRollover}
+              className={cx(
+                'btn btn-secondary',
+                !isPrevPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
+                itemClassName
+              )}
+            >
+              {'<'}
+            </button>
+            {!showAllPages && (
+              <Field
+                name="--pagination-page"
+                value={String(page)}
+                title="Click to change page"
+                displayValue={(v) => `${v} / ${maxPage}`}
+                set={goToAttempt}
                 className={cx(
-                  'btn btn-secondary',
-                  v === page && cx('bg-secondary disabled', activeItemClassName),
-                  itemClassName
+                  'uno-layer-o:(h-full py-0 hover-none inline-block)',
+                  'uno-layer-o:(my-auto w-fit)'
                 )}
-              >
-                {v}
-              </button>
+                inputClassName="uno-layer-o:w-10ch"
+                readOnly={maxPage <= 1}
+              />
+            )}
+            {showAllPages &&
+              pageNumbers.map((v) => (
+                <button
+                  key={v}
+                  tabIndex={0}
+                  data-id={v}
+                  onClick={onPageNumberClick}
+                  disabled={v === page}
+                  className={cx(
+                    'btn btn-secondary',
+                    v === page && cx('bg-secondary disabled', activeItemClassName),
+                    itemClassName
+                  )}
+                >
+                  {v}
+                </button>
+              ))}
+            <button
+              key="next"
+              tabIndex={0}
+              onClick={goNext}
+              disabled={!isNextPossible && !isRollover}
+              className={cx(
+                'btn btn-secondary',
+                !isNextPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
+                itemClassName
+              )}
+            >
+              {'>'}
+            </button>
+            <button
+              key="last"
+              tabIndex={0}
+              onClick={goLast}
+              disabled={!isNextPossible && !isRollover}
+              className={cx(
+                'btn btn-secondary',
+                !isNextPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
+                itemClassName
+              )}
+            >
+              {'>>'}
+            </button>
+          </div>
+        )}
+        <div className="my-auto ml-auto h-full">
+          Page size
+          <select
+            className="ml-2 h-inherit card-secondary"
+            onChange={(e) => setPageSize(e.currentTarget.value)}
+            value={pageSize}
+          >
+            {pageSizes.map((n) => (
+              <option key={n} value={n}>
+                {n}
+              </option>
             ))}
-          <button
-            key="next"
-            tabIndex={0}
-            onClick={goNext}
-            disabled={!isNextPossible && !isRollover}
-            className={cx(
-              'btn btn-secondary',
-              !isNextPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
-              itemClassName
-            )}
-          >
-            {'>'}
-          </button>
-          <button
-            key="last"
-            tabIndex={0}
-            onClick={callbacks.goLast}
-            disabled={!isNextPossible && !isRollover}
-            className={cx(
-              'btn btn-secondary',
-              !isNextPossible && !isRollover && cx('disabled opacity-30', disabledItemClassName),
-              itemClassName
-            )}
-          >
-            {'>>'}
-          </button>
+          </select>
         </div>
-      )}
-    </Fragment>
+      </section>
+    )
   );
 };
 
