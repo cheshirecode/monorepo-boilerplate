6d5b662ddb6c82ac665cb08606b31478cfc62c9c
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 20:52:49 +0800

    Update usePagination.ts

--- packages/app-1/src/components/Pagination/usePagination.ts
+++ packages/app-1/src/components/Pagination/usePagination.ts
@@ -1,11 +1,13 @@
+import { isEqual, isFunction } from 'lodash-es';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
+import usePrevious from '@/services/hooks/usePrevious';
 import { getIntervals } from '@/utils';
 
 import type { PaginationHookParams, PaginationHookResults, PaginationParams } from './typings';
 
 export const DEFAULT_PAGINATION_THRESHOLD = 10;
-export const DEFAULT_PAGE_SIZE_COUNTS = 4;
+export const DEFAULT_PAGE_SIZE_COUNTS = 3;
 
 /**
  * main logic to generate pagination parameters with clamped internvals and resolved page values
@@ -18,45 +20,41 @@ const createNewParams: (
 ) => PaginationParams & Required<Pick<PaginationHookParams, 'page'>> = ({
   page,
   count,
-  pageSize = DEFAULT_PAGINATION_THRESHOLD,
-  _pageSize
+  pageSize,
+  _pageSize = 0,
+  pageSizeCount
 }) => {
   if (count < 0) {
     // eslint-disable-next-line no-console
     console.error('pageSize', pageSize, 'count', count);
     throw RangeError('invalid props, both pageSize and count need to be integers');
   }
-
   /**
    * pageSize is +ve integer, use it
    * pageSize === false - use the full list
    * pageSize !== false and not +ve integer, take the 1st page size option
    * */
-  const threshold = Math.max(count, DEFAULT_PAGINATION_THRESHOLD);
-  const intervals = getIntervals(
-    getIntervals([], threshold).concat(Number(pageSize), Number(_pageSize)),
-    threshold,
-    DEFAULT_PAGE_SIZE_COUNTS
-  );
-  let n = (
-    pageSize === false || pageSize === Math.abs(~~pageSize) ? [~~pageSize || threshold] : intervals
-  )[0];
-  n = Math.max(n, intervals[0]);
-  // compute page size options for easy references
-  const pageSizes =
-    pageSize === Math.abs(~~pageSize)
-      ? getIntervals(
-          [...new Set(intervals.concat(n, Number(_pageSize)).filter((x) => !!x))],
-          threshold,
-          DEFAULT_PAGE_SIZE_COUNTS
-        )
-      : intervals;
+  const resolvedPageSize = ~~pageSize;
+  const maxSize = Math.max(count, resolvedPageSize, DEFAULT_PAGINATION_THRESHOLD);
+  const pageSizes = [
+    ...new Set(
+      getIntervals(
+        getIntervals([], maxSize).concat(resolvedPageSize, _pageSize, DEFAULT_PAGINATION_THRESHOLD),
+        [DEFAULT_PAGINATION_THRESHOLD, maxSize],
+        pageSizeCount
+      )
+    )
+  ];
+  const finalPageSize =
+    pageSizes.find((x) => x === resolvedPageSize) ||
+    pageSizes.find((x) => x === _pageSize) ||
+    pageSizes[0] ||
+    DEFAULT_PAGINATION_THRESHOLD;
 
-  const maxPage = Math.max(Math.ceil(count / n), 1);
-  const newPage = Math.max(Math.min(Number(page), maxPage), 1);
-  const first = Math.min((newPage - 1) * n, count);
-  const last = Math.max(Math.min(count, first + n) - 1, maxPage ? 1 : 0);
-
+  const maxPage = finalPageSize ? Math.max(Math.ceil(count / finalPageSize), 1) : 0;
+  const newPage = Math.max(Math.min(~~page, maxPage), 1);
+  const first = Math.min((newPage - 1) * finalPageSize, count);
+  const last = Math.max(Math.min(count, first + finalPageSize) - 1, maxPage ? 1 : 0);
   return {
     page: newPage,
     count,
@@ -66,21 +64,34 @@ const createNewParams: (
     pageNumbers: Array(maxPage)
       .fill(0)
       .map((_v, i) => i + 1),
-    pageSize: n,
+    pageSize: finalPageSize,
     pageSizes
   } as const;
 };
 
 const usePagination: (p: PaginationHookParams) => PaginationHookResults = (props) => {
-  const { page: initialPage = 1, pageSize, count, onChange, isRollover = false } = props ?? {};
+  const {
+    page: initialPage = 1,
+    pageSize,
+    pageSizeCount: _pageSizeCount,
+    count,
+    onChange,
+    isRollover = false
+  } = props ?? {};
+  const pageSizeCount = useMemo(
+    () => DEFAULT_PAGE_SIZE_COUNTS + Math.max(1, Math.ceil(Math.log10(count))),
+    [count]
+  );
   const [innerParams, setInnerParams] = useState(
     createNewParams({
       page: initialPage,
       count,
       pageSize,
-      _pageSize: pageSize
+      _pageSize: ~~pageSize,
+      pageSizeCount
     })
   );
+  const prevPageSize = usePrevious(innerParams?.pageSize);
   const setParams = useCallback<PaginationHookResults['setParams']>(
     (params: typeof innerParams) => {
       // need to re-create new params based on inputs thus onChange needs to be called inside
@@ -88,15 +99,20 @@ const usePagination: (p: PaginationHookParams) => PaginationHookResults = (props
         const newParams = createNewParams({
           ...p,
           ...params,
-          _pageSize: pageSize
+          _pageSize: ~~prevPageSize,
+          pageSizeCount
         });
-        if (typeof onChange === 'function') {
+        // skip updates if no new chanages
+        if (isEqual(p, newParams)) {
+          return p;
+        }
+        if (isFunction(onChange)) {
           onChange(newParams);
         }
         return newParams;
       });
     },
-    [onChange, pageSize]
+    [onChange, pageSizeCount, prevPageSize]
   );
 
   const helpers: PaginationHookResults = useMemo(() => {
@@ -111,11 +127,11 @@ const usePagination: (p: PaginationHookParams) => PaginationHookResults = (props
       isNextPossible: isRollover || page < maxPage,
       isPrevPossible: isRollover || page > 1,
       isRollover,
-      isTotalLessThanMinPageSize: count < pageSizes[0],
+      isPaginationRedundant: !pageSizes?.length || count <= pageSizes[0],
       onPageNumberClick: (e) => setParams({ page: Number(e.currentTarget.dataset.id) }),
       goTo,
       goToAttempt: (v: unknown) => {
-        const newV = pageNumbers.find((x) => x === Number(v)) ?? pageNumbers[0];
+        const newV = pageNumbers.find((x) => x === ~~v) ?? pageNumbers[0];
         goTo(newV);
         return newV;
       },
@@ -127,7 +143,7 @@ const usePagination: (p: PaginationHookParams) => PaginationHookResults = (props
       },
       goFirst: () => goTo(1),
       goLast: () => goTo(maxPage),
-      setPageSize: (pageSize: number) => setParams({ pageSize: Number(pageSize) })
+      setPageSize: (pageSize: number) => setParams({ pageSize: ~~pageSize })
     };
   }, [innerParams, setParams, isRollover, count]);
 
