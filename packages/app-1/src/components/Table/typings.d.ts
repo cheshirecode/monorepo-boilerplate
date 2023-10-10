13e6b9d6361510067d224dba274e460dfa7e9b0b
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 21:02:49 +0800

    Update typings.d.ts

--- packages/app-1/src/components/Table/typings.d.ts
+++ packages/app-1/src/components/Table/typings.d.ts
@@ -4,14 +4,23 @@ import type {
   ColumnDef,
   ColumnHelper,
   Header,
+  HeaderContext,
+  OnChangeFn,
   Row,
+  SortingState,
+  Table,
   TableOptions
 } from '@tanstack/react-table';
-import type { ReactNode } from 'react';
+import type { DispatchWithoutAction, ReactNode } from 'react';
 
 export type InternalTableProps<T> = Partial<TableOptions<T>> & Pick<TableOptions<T>, 'data'>;
 
 export type ExtraInternalTableProps<T> = {
+  headerRenderer?: (
+    props: HeaderContext<T, unknown>,
+    v: string | null,
+    displayX: string | null
+  ) => ReactNode;
   cellRenderer?: (props: CellContext<T, unknown>, v: string | null) => ReactNode;
   subRowRenderer?: (props: Row<T>) => ReactNode;
   createColumnDefs?: (colDefs: ColumnDef<T>[], helper: ColumnHelper<T>) => ColumnDef<T>[];
@@ -20,12 +29,21 @@ export type ExtraInternalTableProps<T> = {
    * and allow custom columns in userland with createColumnDefs (or set to true and use cellRenderer)
    */
   skipNonPrimitiveColumns?: boolean;
+  /**
+   * by default centering the column if id === actions
+   */
+  centeredActionColumn?: boolean;
 };
 
-export type TableHookParams = Omit<
-  TableOptions<T>,
-  'columns' | 'state' | 'onSortingChange' | 'getCoreRowModel' | 'getSortedRowModel'
->;
+export interface CustomCell<T> extends Cell<T, unknown> {
+  id: string;
+  style: {
+    width?: number | undefined;
+  };
+  className: string;
+  render: () => ReactNode | JSX.Element;
+  children?: ReactNode;
+}
 
 export type TableProps<T> = BaseProps &
   InternalTableProps<T> & {
@@ -33,7 +51,45 @@ export type TableProps<T> = BaseProps &
     classNameGetters?: {
       header?: (props: Header<T, unknown>) => string;
       row?: (props: Row<T>) => string;
+      expandedRow?: (props: Row<T>) => string;
       cell?: (props: Cell<T, unknown>) => string;
     };
     tableClassName?: string;
+    defaultStyling?: boolean;
+    setSorting?: OnChangeFn<SortingState>;
+    /**
+     * (experimental) virtualized rows
+     */
+    isVirtual?: boolean;
+    /**
+     * (experimental) virtualized rows
+     */
+    isHeaderHidden?: boolean;
+    /**
+     * (experimental) virtualized rows
+     */
+    isFooterHidden?: boolean;
+    /**
+     * (experimental) virtualized rows
+     */
+    isBodyHidden?: boolean;
+    hasCustomFooter?: boolean;
   };
+
+export type TableCoreProps<T> = {
+  table: Table<T>;
+  sorting: SortingState;
+  rerender: DispatchWithoutAction;
+  rows: ({
+    id: string;
+    className: string;
+    cells: CustomCell<T>[];
+    subRow: {
+      className: string;
+      colSpan: number;
+      children: ReactNode;
+    };
+  } & Row<T>)[];
+  // (exprimental) set to false in order to use div
+  isTableElement?: boolean;
+};
