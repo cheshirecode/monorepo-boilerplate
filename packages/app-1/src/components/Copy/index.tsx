ac2e59e93d470341d4697fab778df0a52be32765
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 20:46:21 +0800

    Create index.tsx

---
+++ packages/app-1/src/components/Copy/index.tsx
@@ -0,0 +1,17 @@
+import cx from 'classnames';
+import stringify from 'fast-json-stable-stringify';
+import { isString } from 'lodash-es';
+
+import createOnClickCopyToClipboard from '@/services/browser/createOnClickCopyToClipboard';
+
+const Copy = ({ data, className, children, ...rest }: BaseProps & { data: unknown }) => (
+  <button
+    className={cx('btn self-center cursor-pointer flex justify-center items-center', className)}
+    onClick={createOnClickCopyToClipboard(isString(data) ? data : stringify(data))}
+    {...rest}
+  >
+    {children ? children : null}
+  </button>
+);
+
+export default Copy;
