b9cd3919cf5cf329ebf959b17e38e93015d3c81f
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 21:09:25 +0800

    Update useQueryStringForm.ts

--- packages/app-1/src/features/QueryStringForm/useQueryStringForm.ts
+++ packages/app-1/src/features/QueryStringForm/useQueryStringForm.ts
@@ -1,45 +1,38 @@
 import { isFunction } from 'lodash-es';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
-import useInitialEffect from '@/services/hooks/useInitialEffect';
-import { createUrlSearchParams } from '@/services/routes';
+import createUrlSearchParams from '@/services/routes/createUrlSearchParams';
 
-import { QueryStringFormProps, QueryStringHookResults } from './typings';
+import { QueryStringFormProps } from './typings';
 
 const useQueryStringForm = ({
-  queryString,
-  onChange,
-  persistState = false
-}: QueryStringFormProps): QueryStringHookResults => {
-  const [queryStr, setQueryStr] = useState(queryString);
-  const { q, searchParams, createSetter } = useMemo(() => {
-    const q = createUrlSearchParams(queryStr);
+  onQsChange,
+  onKeyValueChange,
+  onParamsChange,
+  data
+}: QueryStringFormProps) => {
+  const { q, createSetter } = useMemo(() => {
+    const q = createUrlSearchParams('', data);
     return {
       q,
       // last write wins - k=0&k1=1&k2=2&k=3 > {k: '3', k1: '1', k2: '2'}
-      searchParams: Object.fromEntries(q.entries()),
       createSetter: (k: string) => (v: string | number) => {
         q.set(k, v);
-        const qs = q.toString();
-        if (isFunction(onChange)) {
-          onChange(qs);
+        if (isFunction(onQsChange)) {
+          onQsChange(q.toString());
         }
-        setQueryStr(qs);
+        if (isFunction(onKeyValueChange)) {
+          onKeyValueChange(k, v);
+        }
+        if (isFunction(onParamsChange)) {
+          onParamsChange(q.entriesAsObj());
+        }
       }
     };
-  }, [onChange, queryStr]);
-  // if props change, update unless persistState
-  useInitialEffect(() => {
-    if (queryString !== queryStr && !persistState) {
-      setQueryStr(queryString);
-    }
-  }, [queryString]);
+  }, [data, onKeyValueChange, onParamsChange, onQsChange]);
 
   return {
-    queryStr,
-    setQueryStr,
     q,
-    searchParams,
     createSetter
   } as const;
 };
