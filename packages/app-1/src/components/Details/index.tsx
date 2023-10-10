def64b38bbddcdef17ffb751f1664a9952c52728
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 20:49:36 +0800

    Update index.tsx

--- packages/app-1/src/components/Details/index.tsx
+++ packages/app-1/src/components/Details/index.tsx
@@ -1,94 +1,183 @@
 import styled from '@emotion/styled';
 import cx from 'classnames';
-import { isNil, isPlainObject, isString, mergeWith } from 'lodash-es';
-import { Fragment } from 'react';
+import { isEmpty, isFunction, isPlainObject, isString, mergeWith } from 'lodash-es';
+import { Fragment, useState } from 'react';
 
+import Card from '@/components/Card';
 import createOnClickCopyToClipboard from '@/services/browser/createOnClickCopyToClipboard';
+import { isEmptyObject } from '@/utils';
 
-import type { DetailsProps } from './typings';
+import type { DetailsMetadata, DetailsProps } from './typings';
 
 const StyledArticle = styled.article``;
-const Details = (props: DetailsProps) => {
+const Details = <T,>(props: DetailsProps<T>) => {
   const {
     className,
+    contentClassName,
     labelClassName,
     fieldClassName,
     fieldCopy,
     data = {},
     metadata = {},
     responsiveGrid = true,
+    containerQueryGrid = false,
+    oneFieldPerLine,
+    heading = '',
+    opened: _opened = false,
+    padding = true,
+    border = true,
+    borderPalette,
+    keyFormatter,
+    valueFormatter,
+    children,
     ...rest
   } = props;
-
+  const [opened, setOpened] = useState(_opened);
+  if (!children && isEmptyObject(data)) {
+    return null;
+  }
   return (
-    <StyledArticle
-      className={cx('', responsiveGrid && 'grid responsive-grid-kv gap-2', className)}
-      {...rest}
+    <Card
+      className={cx(
+        border && [
+          'border',
+          !borderPalette && 'border-primary',
+          borderPalette && `border-${borderPalette}`
+        ],
+        'contain-content',
+        '@container',
+        className
+      )}
     >
-      {Object.keys(data).map((k) => {
-        // resolution order - key > * > null
-        const { label, field } = mergeWith(
-          {},
-          metadata['*'] ?? {},
-          metadata[k] ?? {},
-          (objValue, srcValue, key) => {
-            if (key === 'className') {
-              return cx(objValue, srcValue);
-            }
-          }
-        );
-        const v = data[k];
-        // for object or array types, requires a custom renderer
-        if ((Array.isArray(v) || isPlainObject(v)) && !field?.render) {
-          return null;
-        }
-
-        const renderedKey = label?.render
-          ? label?.render(k, { k, v }, props)
-          : k.toLocaleUpperCase();
-        const renderedValue = field?.render ? field?.render(v, { k, v }, props) : v;
-        const displayValue = field?.render ? '' : v?.toString();
-        if ([renderedValue, renderedKey].every(isNil)) {
-          return null;
-        }
-        const isFieldCopyPossible = fieldCopy && !isNil(displayValue);
-        return (
-          <Fragment key={k}>
-            {label?.fullLinePre && <span className="col-span-full" />}
-            <span
-              className={cx(
-                'text-right border border-transparent',
-                'color-secondary truncate opacity-60',
-                label?.className,
-                labelClassName
-              )}
-              {...(isString(renderedKey) ? { title: renderedKey } : {})}
-            >
-              {renderedKey}
-            </span>
-            {field?.fullLinePre && <span className="col-span-full" />}
-            <span
-              className={cx(
-                '',
-                'color-primary truncate',
-                field?.className,
-                isFieldCopyPossible &&
-                  'cursor-copy border border-transparent @hover:(border-primary)',
-                fieldClassName
-              )}
-              {...(displayValue ? { title: displayValue } : {})}
-              {...(isFieldCopyPossible
+      {heading && (
+        <Card
+          type="information"
+          flex={false}
+          hover={false}
+          className={cx('flex p-1 font-gs-heading05', 'pre-wrap')}
+        >
+          <span className="h-fit my-auto flex-1 flex items-center">{heading}</span>
+          <span
+            name={opened ? 'arrow-drop-up' : 'arrow-drop-down'}
+            children={opened ? '^' : '⌄'}
+            className="my-auto ml-auto btn btn-compact btn-primary"
+            onClick={() => setOpened((v) => !v)}
+            onKeyUp={(e) => e.key === 'Enter' && setOpened((v) => !v)}
+            role="button"
+            tabIndex={0}
+            size="sm"
+          />
+        </Card>
+      )}
+      <StyledArticle
+        className={cx(
+          heading && !opened && 'hidden',
+          (!heading || opened) && (responsiveGrid === true ? 'responsive-grid-kv' : responsiveGrid),
+          (!heading || opened) && containerQueryGrid && '@grid-kv',
+          padding && 'p-1',
+          padding && responsiveGrid && 'gap-1',
+          contentClassName
+        )}
+        {...rest}
+      >
+        {children ??
+          Object.keys(data).map((k) => {
+            // resolution order - key > * > null
+            const { label, field } = mergeWith(
+              oneFieldPerLine
                 ? {
-                    onClick: createOnClickCopyToClipboard(displayValue, { preventDefault: true })
+                    label: {
+                      className: 'col-span-1 '
+                    },
+                    field: {
+                      className: 'col-start-2 col-end--1'
+                    }
                   }
-                : {})}
-            >
-              {renderedValue}
-            </span>
-          </Fragment>
-        );
-      })}
-    </StyledArticle>
+                : {},
+              metadata['*'] ?? {},
+              metadata[k] ?? {},
+              (objValue, srcValue, key) => {
+                if (key === 'className') {
+                  return cx(objValue, srcValue);
+                }
+              }
+            ) as DetailsMetadata<T>;
+            const v = data[k];
+            // for object or array types, requires a custom renderer
+            if ((Array.isArray(v) || isPlainObject(v)) && !field?.render) {
+              return null;
+            }
+            // localisation
+            const localisedKey = isFunction(keyFormatter) ? keyFormatter(k) : k;
+            const localisedValue = isFunction(valueFormatter)
+              ? valueFormatter({
+                  k,
+                  v
+                }) ?? v
+              : v;
+
+            let renderedKey = label?.render
+              ? label?.render(localisedKey, { k, v }, props)
+              : localisedKey;
+            renderedKey =
+              renderedKey && label?.renderAsLabel ? (
+                <label htmlFor={`${label?.renderAsLabel}${k}`}>
+                  {localisedKey}
+                  {localisedKey !== renderedKey && renderedKey}
+                </label>
+              ) : (
+                renderedKey
+              );
+            const renderedValue = field?.render
+              ? field?.render(localisedValue, { k, v }, props)
+              : localisedValue;
+            const displayValue = field?.render ? '' : v?.toString();
+            if ([renderedValue, renderedKey].every(isEmpty)) {
+              return null;
+            }
+            const isFieldCopyPossible = fieldCopy && !isEmpty(displayValue);
+            return (
+              <Fragment key={k}>
+                {label?.fullLinePre && <span className="col-span-full" />}
+                <span
+                  className={cx(
+                    'flex',
+                    (responsiveGrid || containerQueryGrid) && 'justify-end lt-xs:(justify-start)',
+                    'border border-transparent',
+                    'color-secondary opacity-60',
+                    label?.className,
+                    labelClassName
+                  )}
+                  {...(isString(renderedKey) ? { title: renderedKey } : {})}
+                >
+                  {renderedKey}
+                </span>
+                {field?.fullLinePre && <span className="col-span-full" />}
+                <span
+                  className={cx(
+                    'px-1',
+                    'border border-transparent',
+                    'color-primary truncate',
+                    field?.className,
+                    isFieldCopyPossible && 'cursor-copy @hover:(border-primary)',
+                    fieldClassName
+                  )}
+                  {...(displayValue ? { title: displayValue } : {})}
+                  {...(isFieldCopyPossible
+                    ? {
+                        onClick: createOnClickCopyToClipboard(displayValue, {
+                          preventDefault: true
+                        })
+                      }
+                    : {})}
+                >
+                  {renderedValue}
+                </span>
+              </Fragment>
+            );
+          })}
+      </StyledArticle>
+    </Card>
   );
 };
 
