08715eef139eefe4f4bf0a676d9d9a0253c4157b
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 20:38:32 +0800

    Update alias.ts

--- packages/app-1/alias.ts
+++ packages/app-1/alias.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
 
 const alias: Record<string, string> = {
-  '@': resolve(__dirname, './src')
+  '~': resolve(__dirname, './'),
+  '@': resolve(__dirname, './src'),
+  lodash: 'lodash-es'
 };
 
 export default alias;
