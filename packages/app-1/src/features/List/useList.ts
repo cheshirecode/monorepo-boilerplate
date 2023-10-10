1c64b22153ba09556278d8b169ae6f195574010b
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 21:07:56 +0800

    Update useList.ts

--- packages/app-1/src/features/List/useList.ts
+++ packages/app-1/src/features/List/useList.ts
@@ -1,8 +1,10 @@
+import { isFunction, isNumber } from 'lodash-es';
 import { useMemo, useState } from 'react';
 
-import usePagination, { DEFAULT_PAGINATION_THRESHOLD } from '@/components/Pagination/usePagination';
+import usePagination from '@/components/Pagination/usePagination';
+import useSorting from '@/components/Table/useSorting';
 
-import type { ListParams } from './typings';
+import type { ListParams, ListResults } from './typings';
 
 const noOp = <T>(x: T) => x;
 /**
@@ -11,10 +13,10 @@ const noOp = <T>(x: T) => x;
  *  a giant blob that handles everything OR repeated code for with the text filtering and pagination of different arrays
  * TBD - sorting
  * @param arr unknown[] uniform array
- * @param params 
+ * @param params
       filter: {
         str: string;
-        fn?: ((arr: any[], str: string) => any[]) 
+        fn?: ((arr: any[], str: string) => any[])
         onChange?: ((str: string) => void)
       };
       pagination: {
@@ -24,50 +26,96 @@ const noOp = <T>(x: T) => x;
         _pageSize?: number
         onChange?: ((params: Partial<PaginationHookParams>) => void)
         isRollover?: boolean
-      }
+      },
+      sorting: {id, desc}[]
+      setSorting: updater
  * @returns filters/pagination params/filtered then paginated list for users to decide
  */
 const useList = <T>(
   // TODO - fix generics later
   arr: T[],
   params?: ListParams<T>
-) => {
+): ListResults<T> => {
   if (!Array.isArray(arr)) {
-    throw TypeError(`useList - invalid arr type. Received ${typeof arr} instead of Array`, {
-      cause: `${typeof arr} instead of Array`
-    });
+    throw TypeError(
+      `useList - invalid arr type. Received ${typeof arr} - ${arr} instead of Array`,
+      {
+        cause: `${typeof arr} instead of Array`
+      }
+    );
   }
-  const { filter: filterParams, pagination: paginationParams, postProcess } = params ?? {};
-  const [filterStr, setFilterStr] = useState(filterParams?.str ?? '');
+  const {
+    filter: { str = '', onChange, fn = noOp<T[]> } = {},
+    pagination: paginationParams,
+    postProcess = noOp<T[]>,
+    sorting: _sorting = []
+  } = params ?? {};
+  const [filterStr, setFilterStr] = useState(str);
+  // filter out applicable ids (if availble in dataset)
+  const [unsorted, baseSorting] = useMemo(
+    () => [
+      arr.slice(0),
+      arr[0]
+        ? _sorting
+            ?.filter(({ id }) => arr[0] && arr[0][id] !== undefined)
+            .map((f) => ({
+              ...f,
+              _isNumber: isNumber(arr[0][f.id])
+            }))
+        : _sorting
+    ],
+    [_sorting, arr]
+  );
+
+  const [sorting, setSorting] = useSorting(baseSorting);
+
   const filter = useMemo(
     () => ({
       str: filterStr,
       set: (str: string) => {
+        // skip updates if no new chanages
+        if (str === filterStr) {
+          return;
+        }
         setFilterStr(str);
-        if (typeof filterParams?.onChange === 'function') {
-          filterParams?.onChange(str);
+        if (isFunction(onChange)) {
+          onChange(str);
         }
       }
     }),
-    [filterStr, filterParams]
+    [filterStr, onChange]
   );
-  const [filtered, count] = useMemo(() => {
-    const filtered = (filterParams?.fn ?? noOp<T[]>)(arr, filterStr);
-    const count = filtered?.length ?? 0;
-    // pageSize comes from params
 
-    return [filtered, count];
-  }, [arr, filterStr, filterParams?.fn]);
+  const filtered = useMemo(() => {
+    sorting
+      ?.slice(0)
+      ?.reverse()
+      ?.map(({ id, desc, _isNumber }) => {
+        unsorted.sort((a, b) => {
+          if (_isNumber) {
+            return desc ? b[id] - a[id] : a[id] - b[id];
+          }
+          return String(desc ? b[id] : a[id]).localeCompare(desc ? a[id] : b[id]);
+        });
+      });
+
+    return fn(unsorted, filterStr);
+  }, [unsorted, sorting, filterStr, fn]);
 
   const pagination = usePagination({
     ...(paginationParams ?? {}),
     page: paginationParams?.page ?? 1,
-    pageSize: paginationParams?.pageSize ?? DEFAULT_PAGINATION_THRESHOLD,
-    count
+    pageSize: paginationParams === false ? filtered?.length ?? 0 : paginationParams?.pageSize,
+    count: filtered?.length ?? 0
   });
 
   const paginated = useMemo(
-    () => (postProcess ?? noOp<T[]>)(filtered.slice(pagination.first, pagination.last + 1)),
+    () =>
+      postProcess(
+        pagination.last + 1 - pagination.last === filtered.length
+          ? filtered
+          : filtered.slice(pagination.first, pagination.last + 1)
+      ),
     [filtered, pagination.first, pagination.last, postProcess]
   );
 
@@ -75,7 +123,9 @@ const useList = <T>(
     filter,
     pagination,
     filtered,
-    paginated
+    paginated,
+    sorting,
+    setSorting
   } as const;
 };
 
