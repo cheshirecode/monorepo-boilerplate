1117224478b91d3431778112506d99757109ee63
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2023-10-10 21:14:37 +0800

    Update setup.js

--- packages/app-1/src/services/test/setup.js
+++ packages/app-1/src/services/test/setup.js
@@ -1,17 +1,25 @@
+// @ts-nocheck
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: vi.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(), // deprecated
-    removeListener: vi.fn(), // deprecated
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn()
-  }))
-});
+if (typeof window !== 'undefined') {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn()
+    }))
+  });
+}
+
+global.FormData = function () {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  this.append = function () {};
+};
